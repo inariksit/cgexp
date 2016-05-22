4812c0e763152acacf7783b78e05b4bb93edf646
Inari Listenmaa <inari.listenmaa@gmail.com>
2016-05-22 20:47:39 +0200

    add end states

--- Automaton.hs
+++ Automaton.hs
@@ -5,11 +5,12 @@ module Automaton where
 type State = Int
 
 data Automaton a = A { transition :: State -> State -> [a] 
-                     , bound :: State }
+                     , bound :: State 
+                     , final :: State -> Bool }
 
 
 showAutomaton :: (Show a) => Automaton a -> String
-showAutomaton (A f b) = unlines  
+showAutomaton (A f b _) = unlines  
   [       pad ++ show vals  ++ "\n" ++
     show from ++ arrow ++ show to ++ "\n"
     | from <- [0..b]
@@ -25,20 +26,15 @@ showAutomaton (A f b) = unlines
   ]
 
 
-
-
 fromState :: Automaton a -> State -> [(a,State)]
-fromState (A f b) from = [ (a,to) | to <- [0..b]
-                                  , a <- f from to ]
+fromState (A f b _) from = [ (a,to) | to <- [0..b]
+                                    , a <- f from to ]
   
 
 
 toState :: Automaton a -> State -> [(State,a)]
-toState (A f b) to = [ (from,a) | from <- [0..b]
-                                , a <- f from to ]
-
-
-
+toState (A f b _) to = [ (from,a) | from <- [0..b]
+                                  , a <- f from to ]
 
 --------------------------------------------------------------------------------
 
@@ -46,10 +42,13 @@ data Tag = Det | Adj | Noun  deriving (Show,Eq,Enum,Bounded)
 
 
 detAdjNoun :: Automaton Tag
-detAdjNoun = A trans 2
+detAdjNoun = A trans 2 fin
  where 
   trans 0 1 = [Det]
   trans 1 1 = [Adj]
   trans 1 2 = [Adj,Noun]
   trans _ _ = []
+
+  fin 2 = True
+  fin _ = False
 
