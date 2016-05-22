8a9e226cf2c7218fabd5338d6b2230fc54772c4e
Inari Listenmaa <inari.listenmaa@gmail.com>
2016-05-22 20:47:50 +0200

    add removeTo and defaultRules

--- Rule.hs
+++ Rule.hs
@@ -5,8 +5,12 @@ import Data.List
 import Control.Monad ( forM_ )
 
 
-test :: IO ()
-test = [0..2] `forM_` \i -> mapM_ print (removeTagFrom detAdjNoun i)
+test :: Automaton Tag -> IO ()
+test a = do mapM_ print (defRules a)
+            putStrLn ""
+            [0..bound a] `forM_` \i -> mapM_ print (removeTagFrom a i)
+            [1..bound a] `forM_` \i -> mapM_ print (removeTagTo a i)
+
 
 
 --------------------------------------------------------------------------------
@@ -37,43 +41,79 @@ data Context = Yes Position [TagPlus] | No Position [TagPlus] deriving (Eq)
 
 data Position = C Int | NC Int deriving (Eq)
 
-type Cautious = Bool
 
 instance Show Context where
-  show (Yes pos ts) = "(" ++ show pos ++ " " ++ show ts ++ ")"
-  show (No pos ts)  = "(NOT " ++ show pos ++ " " ++ show ts ++ ")"
+  show (Yes pos ts) = "(" ++ show pos ++ " " ++ showTS ts ++ ")"
+  show (No pos ts)  = "(NOT " ++ show pos ++ " " ++ showTS ts ++ ")"
 
 instance Show Position where
   show (C i)  = show i ++ "C"
   show (NC i) = show i
  
 instance Show Rule where
-  show (R trg ctxs) = "REMOVE " ++ show trg ++ " IF " ++ (unwords $ map show ctxs)
+  show (R trg ctxs) = "REMOVE " ++ showTS trg ++ " IF " ++ (unwords $ map show ctxs) ++ " ;"
 
 
+showTS :: [TagPlus] -> String
+showTS  = intercalate " OR " . map show
+
+--------------------------------------------------------------------------------
+
+defRules :: Automaton Tag -> [Rule]
+defRules a = [ R allButStart [nothingInPrevious]
+             , R allButEnd   [nothingInFollowing] ] 
+ where 
+  allButStart = [ TS s | s <- [1..bound a] ]  --all states excluding 0
+  allButEnd   = [ TS s | s <- [0..bound a]
+                       , not $ final a s ]
+  nothingInPrevious = No (NC (-1)) [ T t | t <- [minBound..maxBound] :: [Tag] ]
+  nothingInFollowing = No (NC  1) [ T t | t <- [minBound..maxBound] :: [Tag] ]
 
 --------------------------------------------------------------------------------
 
 removeTagFrom :: Automaton Tag -> State -> [Rule]
-removeTagFrom a s = never:sometimes
+removeTagFrom a s = neverFrom:sometimesFrom
  where
   fromS = fromState a s :: [(Tag,State)]
 
-  --If there is a transition with Det from 0, allow that but remove Det from everywhere else
-  sometimes = [ R [T tag] [beforeCtx,afterCtx]
-                   | (tag,s') <- fromS
-                   , let beforeCtx = Yes (C (-1)) [stateToTag s] 
-                   , let afterCtx = No (NC 1) [stateToTag s'] ]
+
+  -- If there is a transition with Det from 0, 
+  -- allow that but remove Det from everywhere else.
+  sometimesFrom = [ R [T tag] [beforeCtx, afterCtx]
+                    | (tag,s') <- fromS
+                    , let beforeCtx = Yes (C (-1)) [stateToTag s] 
+                    , let afterCtx = No (NC 1) [stateToTag s'] ]
 
 
-  --If there is no transition with Noun from 0, remove all Nouns from 0.
+
+  --If there is no transition with Noun from/to 0, remove all Nouns from/to 0.
   --May not be strictly needed if we go for actual sentences, not symbolic?
-  never = R trg [ctx]
-    where (tags,_) = unzip fromS  --transitions that happen to *some* state
-          trg = map T $ compl tags  --complement: transitions that never happen
-          ctx = Yes (C (-1)) [stateToTag s]
+  neverFrom = R trgFrom [ctxFrom]
+    where (tagsFrom,_) = unzip fromS  --transitions that happen *to* some state
+          trgFrom = map T $ compl tagsFrom  --complement: transitions that never happen
+          ctxFrom = Yes (C (-1)) [stateToTag s]
 
 
 
+removeTagTo :: Automaton Tag -> State -> [Rule]
+removeTagTo a s = neverTo:sometimesTo
+ where
+  toS = toState a s :: [(State,Tag)]
 
---removeState :: Automaton a -> State -> Rule
+  sometimesTo = [ R [T tag] [afterCtx, beforeCtx]
+                  | (s',tag) <- toS
+                  , let afterCtx = Yes (C 1) [stateToTag s]
+                  , let beforeCtx = No (NC (-1)) [stateToTag s'] ]
+
+  neverTo = R trgTo [ctxTo]
+   where (_,tagsTo) = unzip toS  --transitions that happen *from* some state
+         trgTo      = map T $ compl tagsTo
+         ctxTo      = Yes (C 1) [stateToTag s]
+
+--------------------------------------------------------------------------------
+
+removeState :: Automaton a -> State -> Rule
+removeState = undefined
+
+
+
