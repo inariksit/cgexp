f5ad2ac01a36947a7cb95e35532c3987524957b1
Inari Listenmaa <inari.listenmaa@gmail.com>
2016-07-10 20:29:32 +0200

    cleanup

--- Rule.hs
+++ Rule.hs
@@ -19,6 +19,7 @@ main = do
 
 printRules :: Automaton Tag -> IO ()
 printRules a = do mapM_ putStrLn definitions
+                  putStrLn ""
 
                   putStrLn "BEFORE-SECTIONS"
                   mapM_ print (baseRules a)
@@ -27,12 +28,16 @@ printRules a = do mapM_ putStrLn definitions
 
                   putStrLn "SECTION"
                   putStrLn ""
-                  putStrLn "# Remove tags coming from a certain state (-1C foo)"
-                  [0..bound a] `forM_` \i -> mapM_ print (removeTagFrom a i)
                   putStrLn ""
+                  putStrLn "# Remove tags NOT between certain states"
+                  alltags `forM_` \t -> mapM_ print (removeTag a t)
 
-                  putStrLn "# Remove tags coming to a certain state (1C foo)"
-                  [1..bound a] `forM_` \i -> mapM_ print (removeTagTo a i)
+                  --putStrLn "# Remove tags coming from a certain state (-1C foo)"
+                  --[0..bound a] `forM_` \i -> mapM_ print (removeTagFrom a i)
+                  --putStrLn ""
+
+                  --putStrLn "# Remove tags coming to a certain state (1C foo)"
+                  --[1..bound a] `forM_` \i -> mapM_ print (removeTagTo a i)
 
                   putStrLn ""
                   putStrLn "# Remove states between certain tags"
@@ -41,13 +46,18 @@ printRules a = do mapM_ putStrLn definitions
 
 
 --------------------------------------------------------------------------------
--- Ugly hacks
+-- States need to be tags too
 
 data TagPlus = T Tag | TS State | BOS | EOS deriving (Eq)
 
---all normal tags, ie. no states nor BOS/EOS
-alltps :: [TagPlus]
+stateToTag :: State -> TagPlus
+stateToTag = TS  --Makes states also into special tags; 
+                 --these will be inserted between each word.
+
+
+alltps :: [TagPlus] --All normal tags as TagPlus, ie. no states nor BOS/EOS
 alltps = map T alltags
+
 
 instance Show TagPlus where
   show (T tag) = show tag
@@ -55,16 +65,12 @@ instance Show TagPlus where
   show BOS     = ">>>"
   show EOS     = "<<<"
 
-compl :: (Bounded a, Eq a, Enum a) => [a] -> [a]
-compl as = [minBound..maxBound] \\ as
 
-stateToTag :: State -> TagPlus
-stateToTag = TS  --Makes states also into special tags; 
-                 --these will be inserted between each word.
-
+showTS :: [TagPlus] -> String
+showTS = intercalate " OR " . map show 
 
 --------------------------------------------------------------------------------
-
+-- Rules, contexts & positions
 
 data Rule = R { target  :: [TagPlus]
               , context :: [Context] } deriving (Eq)
@@ -74,6 +80,11 @@ data Context = Yes Position [TagPlus] | No Position [TagPlus] deriving (Eq)
 
 data Position = C Int | NC Int deriving (Eq)
 
+-- Shorthands, we'll write these a lot
+c1   = C 1
+c_1  = C (-1)
+nc1  = NC 1
+nc_1 = NC (-1)
 
 instance Show Context where
   show (Yes pos ts) = "(" ++ show pos ++ " " ++ showTS ts ++ ")"
@@ -88,20 +99,18 @@ instance Show Rule where
                       " IF " ++ (unwords $ map show ctxs) ++ " ;"
 
 
-showTS :: [TagPlus] -> String
-showTS = intercalate " OR " . map show 
-
---------------------------------------------------------------------------------
-
-
 definitions :: [String]
 definitions = "SET >>> = (>>>) ;":
               "SET <<< = (<<<) ;":
               [ "SET " ++ (x:xs) ++ " = (" ++ (toLower x:xs) ++ ") ;" 
                 | (x:xs) <- map show alltags ] 
               ++ [ anySet ]
- where anySet = "SET Any = " ++ (intercalate " OR " $ map show alltps) ++ " ;"
+ where anySet = "SET Any = " ++ showTS alltps ++ " ;"
 
+
+
+--------------------------------------------------------------------------------
+-- Automaton to Rules
 
 baseRules :: Automaton Tag -> [Rule]
 baseRules a = [ R allButStart [bos] --[noPrec] 
@@ -110,30 +119,66 @@ baseRules a = [ R allButStart [bos] --[noPrec]
               , R onlyEnd     [hasFoll] ] 
 
  where
-
-
   allButStart = [ TS s | s <- [1..bound a] ] --all states excluding 0 
   allButEnd   = [ TS s | s <- [0..bound a]
                        , not $ final a s ]
 
-  onlyStart --remove start state if it has a preceding cohort;
-            --except if start state is also an accepting state, then keep it
-    = [ TS s | s <- [0] \\ --all states excluding 0 
-                    if final a 0 then [0] else [] ] 
+  --remove start state from all but first (state) cohort;
+  onlyStart = [ TS s | s <- [0] \\ if final a 0
+                                    then [0] --EXCEPT if start also accepts
+                                     else [] ] 
 
+  --remove states that are accepting and sink from all but last (state) cohort
   onlyEnd = [ TS s | s <- [0..bound a]
-                   , final a s ]
+                   , final a s 
+                   , sink a s ]
 
-  noPrec = No (NC (-1)) alltps
-  noFoll = No (NC  1  ) alltps
-  hasPrec = Yes (NC (-1)) alltps
-  hasFoll = Yes (NC   1 ) alltps
+  noPrec = No nc_1 alltps
+  noFoll = No nc1  alltps
+  hasPrec = Yes nc_1 alltps
+  hasFoll = Yes nc1  alltps
 
-  --more readable alternative to noPrec and noFoll
+  --alternative to noPrec and noFoll
   bos = Yes (NC (-1)) [BOS]
-  eos = Yes (NC   1)  [EOS]
+  eos = Yes (NC   0)  [EOS]
+
+---
+
+removeTag :: Automaton Tag -> Tag -> [Rule]
+removeTag a t = [ R [T t] [No nc_1 (map TS froms)]
+                    ,  R [T t] [No nc1  (map TS tos)] ]
+  where (froms,tos) = unzip $ withSymbol a t       
+
+---
+
+--TODO: make it not remove a *final* state when it's not followed by something
+removeState :: Automaton Tag -> State -> [Rule]
+removeState a s = [ R [TS s] [c] 
+                    | c@(No _ (x:xs)) <- contexts] --only non-empty ctxs
+ where
+  (tagsFrom,_) = unzip $ fromState a s
+  (_,tagsTo)   = unzip $ toState a s 
+  contexts     = [ No (NC 1) (map T $ nub tagsFrom) 
+                 , No (NC (-1)) (map T $ nub tagsTo) ] 
+
 
 --------------------------------------------------------------------------------
+-- General utilities
+
+compl :: (Bounded a, Eq a, Enum a) => [a] -> [a]
+compl as = [minBound..maxBound] \\ as
+
+
+--mergeFst [(a,1), (b,2), (c,1), (d,2)] = [([a,c],1),([b,d],2)] 
+mergeFst :: (Ord b, Eq b) => [(a,[b])] -> [([a],[b])]
+mergeFst a_bs = [ (as, bs) 
+                  | as_bs <- groupBy (\(_,b) (_,b') -> b==b') $ sortOn snd a_bs -- :: [[(a,[b])]]
+                  , let as = map fst as_bs
+                  , let bs = snd $ head as_bs 
+                ]
+
+--------------------------------------------------------------------------------
+-- Old, unused, TODO make sure it's not actually needed and delete
 
 {-   Desired behaviour:
     REMOVE Noun IF (-1C (s1)) (NOT 1 (s2) OR (s3)) ;
@@ -146,13 +191,13 @@ baseRules a = [ R allButStart [bos] --[noPrec]
     REMOVE Det IF (-1C (s0)) (NOT 1 (s3)) ;
     REMOVE Noun IF (-1C (s0)) (NOT 1 (s3)) ;
     REMOVE Verb IF (-1C (s0)) (NOT 1 (s3)) ;
--}
+
 
 removeTagFrom :: Automaton Tag -> State -> [Rule]
-removeTagFrom = removeTag transition (C (-1)) (NC 1)
+removeTagFrom = removeTag transitionFrom c_1 nc1
 
 removeTagTo :: Automaton Tag -> State -> [Rule]
-removeTagTo = removeTag transitionTo (C 1) (NC (-1))
+removeTagTo = removeTag transitionTo c1 nc_1
 
 
 removeTag :: (Automaton Tag -> State -> Tag -> [State])
@@ -184,31 +229,4 @@ removeTag tr posFrom posTo a s = never:sometimes
                 | (tags,ss) <- mergeFst byTag
                 , let trg = map T tags
                 , let ctxTo = No posTo (map stateToTag ss) ]
-
-
---------------------------------------------------------------------------------
-
-removeState :: Automaton Tag -> State -> [Rule]
-removeState a s = [ R [TS s] [c] 
-                    | c@(No _ (x:xs)) <- contexts] --only non-empty ctxs
- where
-  (tagsFrom,_) = unzip $ fromState a s
-  (_,tagsTo)   = unzip $ toState a s 
-  contexts     = [ No (NC 1) (map T $ nub tagsFrom) 
-                 , No (NC (-1)) (map T $ nub tagsTo) ] 
-         
---------------------------------------------------------------------------------
-
-{-
-mergeFst [('a',[1,2,3]), ('b',[1,2]), ('c',[1,2,3]), ('d',[1,2])]
-[(['b','d'],[1,2]),(['a','c'],[1,2,3])]
-
 -}
-
-mergeFst :: (Ord b, Eq b) => [(a,[b])] -> [([a],[b])]
-mergeFst a_bs = [ (as, bs) 
-                  | as_bs <- groupBy (\(_,b) (_,b') -> b==b') $ sortOn snd a_bs -- :: [[(a,[b])]]
-                  , let as = map fst as_bs
-                  , let bs = snd $ head as_bs 
-                ]
-
