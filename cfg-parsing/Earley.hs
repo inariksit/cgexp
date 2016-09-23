4ffe24e9071e692e3f87872e62e60a33d0fe0ea6
Inari Listenmaa <inari.listenmaa@gmail.com>
2016-09-23 17:26:52 +0200

    stuff

--- cfg-parsing/Earley.hs
+++ cfg-parsing/Earley.hs
@@ -1,32 +1,36 @@
 module Earley where 
 
 import Data.List (intercalate)
+import qualified Data.Set as S
 import qualified Data.IntMap as IM
 import Data.IntMap ((!))
+
 
 type Grammar = [Production]
 type Sentence = [String]
 
 data Production = (:->) { lhs :: Nonterminal
-                        , rhs :: [RHS] } --different options of consecutive symbols
+                        , rhs :: RHS } --different options of consecutive symbols
 
-data Nonterminal = S | N String -- S is special; beside that, any other letter or word
+data Nonterminal = S | NP | VP | PP | POS String deriving (Eq) -- S is special; beside that, any other letter or word
 type Terminal = String -- anything goes
-data Symbol = T Terminal | NT Nonterminal -- To have terminals and nonterminals in the same list
+data Symbol = T Terminal | NT Nonterminal deriving (Eq) -- To have terminals and nonterminals in the same list
 
 type RHS = [Symbol] -- Any consecutive symbols
 
 data State = State { prod :: Production 
-                   , dot :: Int --where in the rhs-list are we reading currently. 
-                   , blocksProd :: Maybe Int } -- Nothing: the state is there in the final analysis of the string
+                   , orig :: Int -- where the constituent predicted by this state begins
+                   , dot :: Int -- where in the RHS of the prod are we reading currently 
+                   , blocksProd :: Maybe Int } deriving (Show)
+         									   -- Nothing: the state is there in the final analysis of the string
                                                -- Just x: word in index x blocks this state from happening
 
 zeroState :: Grammar -> [State]
-zeroState gr = [State startProd 0 Nothing]
-  where [startProd] = filter start gr
+zeroState gr = [ State sp 0 0 Nothing | sp <- filter start gr ]
 
 
-type Chart = IM.IntMap [State]
+
+type Chart = IM.IntMap [State] --(S.Set State)
 
 --------------------------------------------------------------------------------
 
@@ -36,7 +40,10 @@ instance Show Symbol where
 
 instance Show Nonterminal where
   show S     = "S"
-  show (N x) = x
+  show NP    = "NP"
+  show VP    = "VP"
+  show PP    = "PP"
+  show (POS x) = x
 
 instance Show Production where
   show (lh :-> rh) = show lh ++ "->" ++ intercalate "|" (map show rh)
@@ -51,54 +58,72 @@ finished :: State -> Bool
 finished s = dot s >= length (rhs $ prod s)
 
 next :: State -> Symbol
-next s = (head $ rhs $ prod s) !! dot s
+next s = (rhs $ prod s) !! dot s
 
 terminal :: Symbol -> Bool
 terminal (T _) = True
 terminal _     = False
 
+isPos :: Symbol -> Bool
+isPos (NT (POS _)) = True
+isPos _       = False
+
 --------------------------------------------------------------------------------
 
 earley :: Sentence -> Grammar -> Chart
-earley sent gr = foldl go IM.empty (zip [0..] ("DUMMY":sent))
+earley sent gr = foldl go states (zip [0..] sent)
  where 
-  go :: Chart -> (Int,String) -> Chart
-  go states (0,word) = IM.insert 0 (zeroState gr) states
+  states = IM.insert 0 (zeroState gr) IM.empty
 
-  go states (i,word) = IM.insertWith (++) i (concatMap act latestState) states  
+  go :: Chart -> (Int,String) -> Chart
+  go states (j,word) = IM.insertWith (++) j (concatMap act latestStates) states  
   	
 
    where
-    latestState = states ! (i-1)
+    latestStates = states ! (j-1)
 
     completer :: State -> [State]
     -- For every state in S(k) of the form (X → γ •, j), 
     -- find states in S(j) of the form (Y → α • X β, i) and add (Y → α X • β, i) to S(k).
     completer s = undefined
 
-    scanner :: State -> [State]
-    scanner s = undefined
+    scanner :: State -> [State] -- For every state in S(k) of the form (X → γ • Pos, [i,j]),  
+    scanner s = undefined     -- examine the input `word' to see if it matches the Pos,
+    						   -- then create rule (Pos → word, [j,j+1])
 
     predictor :: State -> [State]
-    predictor s = undefined
+    predictor s = [ State pd d d Nothing | pd <- filter (isLhs (next s)) gr ]
+     where (o,d) = (orig s, dot s)
 
     act :: State -> [State]
-    act s | finished s        = completer s
-          | terminal (next s) = scanner s 
-          | otherwise         = predictor s
+    act s | finished s     = completer s
+          | isPos (next s) = scanner s 
+          | otherwise      = predictor s
 
 --------------------------------------------------------------------------------
 
-np = N "NP"
-vp = N "VP"
-n = N "N"
-v = N "V"
-dt = N "Det"
 
-grammar = [ S  :-> [ [ NT np, NT vp ] ]
-          , np :-> [ [ NT dt, NT n ]  ]
-          , vp :-> [ [ NT v,  NT np ] ]
-          , dt :-> [ [T "the"], [T "a"] ]
-          , n  :-> [ [T "cat"], [T "marks"], [T "essays"] ]
-          , v  :-> [ [T "cat"], [T "marks"], [T "sleeps"] ]
+n = POS "N"
+v = POS "V"
+dt = POS "Det"
+
+grammar = [ S  :-> [NT NP, NT VP]
+          , NP :-> [NT dt, NT n]
+          , NP :-> [NT NP, NT n]
+          , VP :-> [NT v,  NT NP]
+          , VP :-> [NT v]
+          , dt :-> [T "the"]
+          , n  :-> [T "cat"]
+          , n  :-> [T "marks"]
+          , n  :-> [T "essays"]
+          , v  :-> [T "cat"]
+          , v  :-> [T "marks"]
+          , v  :-> [T "sleeps"]
           ]
+
+--pos :: Grammar -> Terminal -> [Symbol]
+--pos gr term = map rhs $ filter isTerm gr
+
+isLhs symb (_ :-> symbs) | symb `elem` symbs = True
+						 | otherwise         = False
+
