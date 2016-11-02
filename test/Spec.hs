09293f09365b22a2ed84025d17e102a0df519866
Inari Listenmaa <inari.listenmaa@gmail.com>
2016-11-02 15:22:02 +0000

    import Automaton

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,9 +1,10 @@
 import Arbitrary
+import Automaton
 import Test.QuickCheck
 
 main :: IO ()
 main = verboseCheck checkAutomaton
 
-
+--TODO: add some actual tests
 checkAutomaton :: Automaton Tag -> Bool
 checkAutomaton a = True
